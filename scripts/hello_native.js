9ff216246ed72754eee64ff3486482a1aed4d2f9
Asma <asmapa789@gmail.com>
2026-05-02 09:50:27 +0100

    lab10

---
+++ scripts/hello_native.js
@@ -0,0 +1,10 @@
+console.log("[+] Script chargé");
+
+const recvPtr = Process.getModuleByName("libc.so").getExportByName("recv");
+console.log("[+] recv trouvée à : " + recvPtr);
+
+Interceptor.attach(recvPtr, {
+  onEnter(args) {
+    console.log("[+] recv appelée");
+  }
+});
